44c136291af6ff229c11ff5de4d9c88c5115132e
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-18 17:21:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -36,6 +36,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
+    binding.pry
     result = @num1.send(@operation, @num2)
     "#{result}"
   end
