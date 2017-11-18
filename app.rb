e50190427f0250e96d4f63e348d5e8b81d0efb43
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-18 17:20:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,10 +16,11 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    #binding.pry
+    output = ""
     @number.times do
-      "#{@phrase}"
+      output += "#{@phrase}\n"
     end
+    "#{output}"
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
